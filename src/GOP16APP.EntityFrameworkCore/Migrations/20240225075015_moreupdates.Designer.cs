7eb72709ac7e8c37603bfdf50e8311ed1e106ffc
saraBadawy77 <107307935+saraBadawy77@users.noreply.github.com>
2024-02-25 10:12:44 +0200

    Addmigration

--- src/GOP16APP.EntityFrameworkCore/Migrations/20240225075015_moreupdates.Designer.cs
+++ src/GOP16APP.EntityFrameworkCore/Migrations/20240225075015_moreupdates.Designer.cs
@@ -4,6 +4,7 @@ using GOP16APP.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace GOP16APP.Migrations
 {
     [DbContext(typeof(GOP16APPDbContext))]
-    partial class GOP16APPDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240225075015_moreupdates")]
+    partial class moreupdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
